4550c63a1d4be1cfd833b2be50b28dabacdc0d9c
Kim Hogle <kim.hogle13@gmail.com>
2022-07-23 20:32:09 -0400

    Updates

--- sass/javascript.js
+++ sass/javascript.js
@@ -2,5 +2,25 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
 }
 
-let x = document.getElementById("hamburgerBtn");
+let x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+
+function getMailtoUrl(to, subject, body) {
+    var args = [];
+    if (typeof subject !== 'undefined') {
+        args.push('subject=' + encodeURIComponent(subject));
+    }
+    if (typeof body !== 'undefined') {
+        args.push('body=' + encodeURIComponent(body))
+    }
+
+    var url = 'mailto:' + encodeURIComponent(to);
+    if (args.length > 0) {
+        url += '?' + args.join('&');
+    }
+    return url;
+}
+
+
+
